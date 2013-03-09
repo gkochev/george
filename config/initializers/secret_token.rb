d76d83496e4e87a09b9abfe0faf39683faf7e504
gkochev <gkochev@gwu.edu>
2013-03-09 16:00:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+George::Application.config.secret_token = 'abb40d1f01bb361a47b7f9e3da79d2b7ac63f107bc2508d3e963f4bfa75f3b9205816c5721979da2aa63f105ac52b2cc3c6ec798384eb40b18397cdf73894acf'
